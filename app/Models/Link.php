7f51d9e8a3952243ab7449c568694f89bd5cc751
muudzo <tatendawalter62@gmail.com>
2025-04-15 19:57:03 +0200

    update  model files

--- app/Models/Link.php
+++ app/Models/Link.php
@@ -2,9 +2,39 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Link extends Model
 {
-    //
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'title',
+        'url',
+        'description',
+        'favicon',
+        'is_active',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'is_active' => 'boolean',
+    ];
+
+   
+    public function categories(): BelongsToMany
+    {
+        return $this->belongsToMany(LinkCategory::class);
+    }
 }
--- app/Models/LinkCategory.php
+++ app/Models/LinkCategory.php
@@ -2,9 +2,31 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class LinkCategory extends Model
 {
-    //
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'slug',
+        'color',
+        'description',
+    ];
+
+    /**
+     * Get the links for the category.
+     */
+    public function links(): BelongsToMany
+    {
+        return $this->belongsToMany(Link::class);
+    }
 }
